a658b0d6636912f5a6caa2cb450eae4ed5df6c64
Latgaleens <90316488+Latgaleens@users.noreply.github.com>
2022-01-11 21:42:39 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,108 @@
+console.log('Script started');
+
+function innit () {
+    console.log('Adding listeners')
+    addListeners();
+    document.getElementById('elements');
+    
+    var clearButton = document.getElementById('clear');
+    clearButton.addEventListener("click", onClickClear);
+    var submitCustom = document.getElementById('generate2');
+    submitCustom.addEventListener("click", onClickCustom);
+}
+
+function onClick() {
+    onClickClear();
+    console.log('Button hit');
+        for(let i = 1; i<101; i++){
+            const element = document.createElement('div');
+            element.className="element" + i;
+            elements.appendChild(element);
+                if (i % 3 ===0 && i % 5 === 0){
+                    element.setAttribute("id", "element" +i);
+                    element.setAttribute("class", "FizzBuzz");
+                    element.innerText="FizzBuzz " + i;
+                }
+                else if (i % 3 === 0 && i % 5 !== 0) {
+                    element.setAttribute("id", "element" +i);
+                    element.setAttribute("class", "Fizz");
+                    element.innerText="Fizz " + i;
+                    
+                }
+
+                else if (i % 5 === 0 && i % 3 !== 0){
+                    element.setAttribute("id", "element" +i);
+                    element.setAttribute("class", "Buzz");
+                    element.innerText="Buzz " + i;
+                    
+                }
+
+                else if (i % 3 !== 0 && i % 5 !== 0){
+                    element.setAttribute("id", "element" +i);
+                    element.setAttribute("class", "Empty");
+                    element.innerText=i;
+                    
+                }
+        }
+}
+
+function onClickClear () {
+    while (elements.firstChild) {
+        elements.removeChild(elements.firstChild);
+    }
+    console.log('Divs cleared');
+}
+
+function onClickCustom (){
+    onClickClear();
+    console.log('Divs cleared');
+    console.log('Generate custom divs');
+    var startValue = document.getElementById('startValue').value;
+    var endValue = document.getElementById('endValue').value;   
+    var fizzValue = document.getElementById('fizzValue').value;
+    var buzzValue = document.getElementById('buzzValue').value;
+    console.log('start value - ' + startValue);
+    console.log('end value - ' + endValue);
+    console.log('fizz value - ' + fizzValue);
+    console.log('buzz value - ' + buzzValue);
+        for (startValue; startValue<=endValue; startValue++){
+            const element = document.createElement('div');
+            element.id="element" + startValue;
+            
+            elements.appendChild(element);
+                if (startValue % fizzValue === 0 && startValue % buzzValue === 0){
+                    element.setAttribute("id", "element" +startValue);
+                    element.setAttribute("class", "FizzBuzz");
+                    element.innerText="FizzBuzz " + startValue;
+                }
+                else if (startValue % fizzValue === 0 && startValue % buzzValue !== 0) {
+                    element.setAttribute("id", "element" +startValue);
+                    element.setAttribute("class", "Fizz");
+                    element.innerText="Fizz " + startValue;
+                }
+
+                else if (startValue % buzzValue === 0 && startValue % fizzValue !== 0){
+                    element.setAttribute("id", "element" +startValue);
+                    element.setAttribute("class", "Buzz");
+                    element.innerText="Buzz " + startValue;
+                }
+
+                else if (startValue % fizzValue !== 0 && startValue % buzzValue !== 0){
+                    element.setAttribute("id", "element" +startValue);
+                    element.setAttribute("class", "Empty");
+                    element.innerText=startValue;
+                }
+        }
+}
+
+ function addListeners() {
+    const submit = document.getElementById('generate');
+    submit.onclick = onClick;
+}
+
+
+
+innit();
+
+
 
